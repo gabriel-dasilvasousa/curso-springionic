e2d5414498db75c8381749995c253efc24d94beb
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-06-20 15:34:12 -0300

    Criando operação de Instaciação

--- src/main/java/com/gabriel/cursospringionic/CursospringionicApplication.java
+++ src/main/java/com/gabriel/cursospringionic/CursospringionicApplication.java
@@ -1,13 +1,33 @@
 package com.gabriel.cursospringionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringionicApplication {
+import com.gabriel.cursospringionic.domain.Categoria;
+import com.gabriel.cursospringionic.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursospringionicApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringionicApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		repo.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
 }
