352f18296af8f0c6d65b4df4884fe68ca309e332
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-06-24 13:39:54 -0300

    Estado e Cidade

---
+++ src/main/java/com/gabriel/cursospringionic/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.gabriel.cursospringionic.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabriel.cursospringionic.domain.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
---
+++ src/main/java/com/gabriel/cursospringionic/repositories/EstadoRepository.java
@@ -0,0 +1,8 @@
+package com.gabriel.cursospringionic.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabriel.cursospringionic.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+}
