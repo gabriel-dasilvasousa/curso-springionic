65fac64a1027d9accdf9c10901a180e66c3ba306
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-06-25 14:56:16 -0300

    Endpoint clientes/{id} disponivel

---
+++ src/main/java/com/gabriel/cursospringionic/services/ClienteService.java
@@ -0,0 +1,28 @@
+package com.gabriel.cursospringionic.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriel.cursospringionic.domain.Cliente;
+import com.gabriel.cursospringionic.repositories.ClienteRepository;
+import com.gabriel.cursospringionic.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
