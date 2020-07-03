68eb3e959325be1f510aeb42b63b5eb1a91254cd
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-07-03 18:00:22 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/gabriel/cursospringionic/CursospringionicApplication.java
+++ src/main/java/com/gabriel/cursospringionic/CursospringionicApplication.java
@@ -1,5 +1,6 @@
 package com.gabriel.cursospringionic;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,37 +13,48 @@ import com.gabriel.cursospringionic.domain.Cidade;
 import com.gabriel.cursospringionic.domain.Cliente;
 import com.gabriel.cursospringionic.domain.Endereco;
 import com.gabriel.cursospringionic.domain.Estado;
+import com.gabriel.cursospringionic.domain.Pagamento;
+import com.gabriel.cursospringionic.domain.PagamentoComBoleto;
+import com.gabriel.cursospringionic.domain.PagamentoComCartao;
+import com.gabriel.cursospringionic.domain.Pedido;
 import com.gabriel.cursospringionic.domain.Produto;
+import com.gabriel.cursospringionic.domain.enums.EstadoPagamento;
 import com.gabriel.cursospringionic.domain.enums.TipoCliente;
 import com.gabriel.cursospringionic.repositories.CategoriaRepository;
 import com.gabriel.cursospringionic.repositories.CidadeRepository;
 import com.gabriel.cursospringionic.repositories.ClienteRepository;
 import com.gabriel.cursospringionic.repositories.EnderecoRepository;
 import com.gabriel.cursospringionic.repositories.EstadoRepository;
+import com.gabriel.cursospringionic.repositories.PagamentoRepository;
+import com.gabriel.cursospringionic.repositories.PedidoRepository;
 import com.gabriel.cursospringionic.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursospringionicApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoriaRepository categoriaRepo;
+	private CategoriaRepository categoriaRepository;
 
 	@Autowired
-	private ProdutoRepository produtoRepo;
+	private ProdutoRepository produtoRepository;
 	
 	@Autowired
-	private EstadoRepository estadoRepo;
+	private EstadoRepository estadoRepository;
 	
 	@Autowired
-	private CidadeRepository cidadeRepo;
+	private CidadeRepository cidadeRepository;
 	
 	@Autowired
-	private ClienteRepository clienteRepo;
+	private ClienteRepository clienteRepository;
 	
 	@Autowired
-	private EnderecoRepository enderecoRepo;
+	private EnderecoRepository enderecoRepository;
 	
+	@Autowired 
+	private PedidoRepository pedidoRepository;
 	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringionicApplication.class, args);
@@ -65,8 +77,8 @@ public class CursospringionicApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
@@ -78,8 +90,8 @@ public class CursospringionicApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
-		estadoRepo.saveAll(Arrays.asList(est1, est2));
-		cidadeRepo.saveAll(Arrays.asList(c1, c2, c3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
@@ -90,9 +102,24 @@ public class CursospringionicApplication implements CommandLineRunner {
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 		
-		clienteRepo.saveAll(Arrays.asList(cli1));
-		enderecoRepo.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
