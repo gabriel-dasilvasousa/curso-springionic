fef3b90f6a5c620fb987b40bc47a0c634e26efe7
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-07-04 14:46:25 -0300

    Atualizando Categoria com Put, outro pequeno ajuste

--- src/main/java/com/gabriel/cursospringionic/resources/CategoriaResource.java
+++ src/main/java/com/gabriel/cursospringionic/resources/CategoriaResource.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ import com.gabriel.cursospringionic.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
-public class CategoriaResources {
+public class CategoriaResource {
 	
 	@Autowired
 	CategoriaService service;
@@ -34,6 +35,13 @@ public class CategoriaResources {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 
 }
