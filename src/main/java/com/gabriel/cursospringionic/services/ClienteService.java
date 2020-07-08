ead184a0c4f35db2fe21df60cfd3273ee4c60e2e
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-07-08 18:21:29 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/gabriel/cursospringionic/services/ClienteService.java
+++ src/main/java/com/gabriel/cursospringionic/services/ClienteService.java
@@ -56,7 +56,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir esse cliente porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir esse cliente porque há pedidos relacionados");
 		}
 	}
 
