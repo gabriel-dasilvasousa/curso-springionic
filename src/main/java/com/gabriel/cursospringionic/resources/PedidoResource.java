2c89343e12ea5cc27ce75d8194f4c310cbf695dd
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-07-28 17:21:10 -0300

    Criando profile de teste

--- src/main/java/com/gabriel/cursospringionic/resources/PedidoResource.java
+++ src/main/java/com/gabriel/cursospringionic/resources/PedidoResource.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.gabriel.cursospringionic.domain.Categoria;
 import com.gabriel.cursospringionic.domain.Pedido;
-import com.gabriel.cursospringionic.dto.CategoriaDTO;
 import com.gabriel.cursospringionic.services.PedidoService;
 
 @RestController
