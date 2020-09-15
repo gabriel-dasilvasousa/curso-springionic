e95d1ec3506e329c9065ef55d0edc8cb0d9de349
Gabriel da Silva Sousa <g.silva13455@gmail.com>
2020-09-14 23:02:37 -0300

    Implementando SmtpService com servidor do Google

--- src/main/java/com/gabriel/cursospringionic/config/DevConfig.java
+++ src/main/java/com/gabriel/cursospringionic/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.gabriel.cursospringionic.services.DBService;
+import com.gabriel.cursospringionic.services.EmailService;
+import com.gabriel.cursospringionic.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.instatiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
