73619710b6496042ba97af38574783064b6d47bc
Gabriel da Silva Sousa <g.silva13455@gmail.com>
2020-09-24 21:16:57 -0300

    Implementando autenticacao e geracao de token jwt

---
+++ src/main/java/com/gabriel/cursospringionic/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.gabriel.cursospringionic.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.gabriel.cursospringionic.domain.Cliente;
+import com.gabriel.cursospringionic.repositories.ClienteRepository;
+import com.gabriel.cursospringionic.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfil());
+	}
+
+}
